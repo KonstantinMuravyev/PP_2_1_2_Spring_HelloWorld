1f1b33ed123891b22f8574872cb87fb1a7615cea
KonstantinMuravyev <k.muravyev2016@yandex.ru>
2023-08-25 07:20:48 +1000

    add class Cat, иуbean cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,12 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld firstBean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld secondBean = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(firstBean == secondBean);
+
+        Cat firstCat = (Cat) applicationContext.getBean("cat");
+        Cat secondCat = (Cat) applicationContext.getBean("cat");
+        System.out.println(firstCat == secondCat);
     }
 }
